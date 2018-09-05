7eb086b309631e239f9368c5c236683a37f81a1b
Dennis Dang <dangggdennis@gmail.com>
2018-09-04 19:41:10 -0700

    mdc 103, step 5 wip

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -16,23 +16,59 @@ import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'login.dart';
+import 'colors.dart';
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    accentColor: kShrineBrown900,
+    primaryColor: kShrinePink100,
+    buttonColor: kShrinePink100,
+    scaffoldBackgroundColor: kShrineBackgroundWhite,
+    cardColor: kShrineBackgroundWhite,
+    textSelectionColor: kShrinePink100,
+    errorColor: kShrineErrorRed,
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildShrineTextTheme(base.accentTextTheme),
+  );
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline: base.headline.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        title: base.title.copyWith(fontSize: 18.0),
+        caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
+}
+
+final ThemeData _kShrineTheme = _buildShrineTheme();
 
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shrine',
-      // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
-      home: HomePage(),
-      // TODO: Make currentCategory field take _currentCategory (104)
-      // TODO: Pass _currentCategory for frontLayer (104)
-      // TODO: Change backLayer field value to CategoryMenuPage (104)
-      initialRoute: '/login',
-      onGenerateRoute: _getRoute,
-      // TODO: Add a theme (103)
-    );
+        title: 'Shrine',
+        // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
+        home: HomePage(),
+        // TODO: Make currentCategory field take _currentCategory (104)
+        // TODO: Pass _currentCategory for frontLayer (104)
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+        initialRoute: '/login',
+        onGenerateRoute: _getRoute,
+        theme: _kShrineTheme);
   }
 
   Route<dynamic> _getRoute(RouteSettings settings) {
